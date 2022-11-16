ea0c476b3dcc769ee582d10788085e8ce50ab7e4
SrHatcher <erickomaldonado11@gmail.com>
2022-11-16 12:52:10 -0600

    archivos iniciales e iniciacion de Typescript

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import express, {json, Request, Response} from 'express'
+
+class App{
+
+}
+
+export default App
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import 'dotenv/config'
+import App from './app'
+
+//Inicar aplicacion
+
