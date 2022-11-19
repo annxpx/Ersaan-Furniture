4d134b911ea992fb71821b3ace82a040e7850842
Sary Nog <sary.noguera@unah.hn>
2022-11-18 23:02:26 -0600

    initialize app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,22 @@
-import express, {json, Request, Response} from 'express'
+import express, {json} from 'express'
 
 class App{
 
+    public express : express.Application;
+
+    constructor(){
+        this.express = express();
+        this.middlewares();
+    }
+
+    middlewares(){
+        this.express.use(json());
+    }
+
+    listen(port : number) {
+        this.express.listen(port, () => 
+        console.log(`Server run in: http://localhost:${port}`))
+    }
 }
 
-export default App
+export default new App();
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,5 @@ import 'dotenv/config'
 import App from './app'
 
 //Inicar aplicacion
+App.listen(process.env.APP_PORT as unknown as number || 3001);
 
